cf82f19d581c73e1731f2859f8b48e36004ca1b8
benj-lazaro <benjie.work@gmail.com>
2024-12-27 19:39:43 +0800

    04 type inference

---
+++ 03-type-annotation/04-type-inference/code/variables.ts
@@ -0,0 +1,13 @@
+// Type Inference
+
+// Variable of type string
+let movieTitle = "hackers";
+movieTitle.toLocaleUpperCase();
+
+// Variable of type number
+let numCatLives = 9;
+numCatLives += 1;
+
+// Variable of type boolean
+let gameOver = false;
+gameOver = true;

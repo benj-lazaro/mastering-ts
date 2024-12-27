80c0bfd872b7eafb01f2036870bf147f1167977f
benj-lazaro <benjie.work@gmail.com>
2024-12-27 18:01:03 +0800

    03 compiling typescript

---
+++ 03-type-annotation/03-compiling-typescript/code/variables.js
@@ -0,0 +1,10 @@
+// Variable type declaration
+// Variable annotated with type of string
+var movieTitle = "hackers";
+movieTitle.toLocaleUpperCase();
+// Variable annotated with type of number
+var numCatLives = 9;
+numCatLives += 1;
+// Variable annotated with type of boolean
+var gameOver = false;
+gameOver = true;
---
+++ 03-type-annotation/03-compiling-typescript/code/variables.ts
@@ -0,0 +1,13 @@
+// Variable type declaration
+
+// Variable annotated with type of string
+let movieTitle = "hackers";
+movieTitle.toLocaleUpperCase();
+
+// Variable annotated with type of number
+let numCatLives: number = 9;
+numCatLives += 1;
+
+// Variable annotated with type of boolean
+let gameOver: boolean = false;
+gameOver = true;

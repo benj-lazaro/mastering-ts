51d77b999a1704b0921029f154832b6508289722
benj-lazaro <benjie.work@gmail.com>
2024-12-29 17:28:04 +0800

    01 function parameter annotations

---
+++ 04-functions/01-parameter-annotations/code/functions.js
@@ -0,0 +1,9 @@
+// Function Parameter Annotations
+function square(num) {
+    return num * num;
+}
+console.log(square(2));
+var greet = function (person) {
+    return "Hi, ".concat(person, " !");
+};
+console.log(greet("Babayaga"));
---
+++ 04-functions/01-parameter-annotations/code/functions.ts
@@ -0,0 +1,11 @@
+// Function Parameter Annotations
+
+function square(num: number) {
+  return num * num;
+}
+console.log(square(2));
+
+const greet = (person: string) => {
+  return `Hi, ${person} !`;
+};
+console.log(greet("Babayaga"));

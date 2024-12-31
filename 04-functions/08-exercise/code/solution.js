44c17275b0bf3b463ab2283a27b1bd625f0c06ed
benj-lazaro <benjie.work@gmail.com>
2024-12-31 18:54:03 +0800

    08 exercise

---
+++ 04-functions/08-exercise/code/solution.js
@@ -0,0 +1,28 @@
+// Exercise #1:
+// Write a function called "twoFer" that accepts a person's name
+// It should return a string in the format "one for <name>, one for me"
+// If no name is provided, it should default to "you"
+function twoFer(person) {
+    if (person === void 0) { person = "you"; }
+    return "One for ".concat(person, ", one for me");
+}
+var result = twoFer("John Wick");
+console.log(result);
+result = twoFer();
+console.log(result);
+// Exercise #2:
+// Write a isLeapyear() function that accepts a year and returns true/false depending on if the year is a leap year
+// isLeapYear(2012) => true
+// isLeapYear(2013) => false
+function isLeapYear(year) {
+    //   if (year % 4 === 0 && year % 100 !== 0) return true;
+    //   else if (year % 400 === 0) return true;
+    //   return false;
+    return (year % 4 === 0 && year % 100 !== 8) || year % 400 === 0;
+}
+var leapYear = isLeapYear(2012);
+console.log(leapYear);
+leapYear = isLeapYear(2013);
+console.log(leapYear);
+leapYear = isLeapYear(2025);
+console.log(leapYear);

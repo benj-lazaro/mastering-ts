f8afd6e5fb91ce9942acece4a89af850d2897435
benj-lazaro <benjie.work@gmail.com>
2024-12-31 17:41:15 +0800

    06 the void type

---
+++ 04-functions/06-void-type/code/functions.js
@@ -0,0 +1,8 @@
+// Void Type
+// Explicitly annotated type void
+function printTwice(msg) {
+    console.log(msg);
+    console.log(msg);
+    return "Hey";
+}
+printTwice("Hello!");
---
+++ 04-functions/06-void-type/code/functions.ts
@@ -0,0 +1,9 @@
+// Void Type
+
+// Explicitly annotated type void
+function printTwice(msg: string): void {
+  console.log(msg);
+  console.log(msg);
+}
+
+printTwice("Hello!");

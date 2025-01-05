b061c02d694ff62b28c28b090108207c4c4f5f92
benj-lazaro <benjie.work@gmail.com>
2025-01-05 17:17:14 +0800

    01 working with array types

---
+++ 06-array-types/01-working-with-array-types/code/array-types.js
@@ -0,0 +1,14 @@
+// Array Types
+// Constant activeUsers is annotated as an array of string & assigned with an empty array
+var activeUsers = [];
+// Push new items into the array
+activeUsers.push("John Wick");
+activeUsers.push("John Constantine");
+activeUsers.push("Thomas Anderson");
+console.log(activeUsers);
+// Constant agelist ia annotated as an array of numbers with initial items
+var ageList = [45, 56, 13];
+console.log(ageList);
+ageList.pop();
+ageList.push(47);
+console.log(ageList);
---
+++ 06-array-types/01-working-with-array-types/code/array-types.ts
@@ -0,0 +1,18 @@
+// Array Types
+
+// Constant activeUsers is annotated as an array of string & assigned with an empty array
+const activeUsers: string[] = [];
+
+// Push new items into the array
+activeUsers.push("John Wick");
+activeUsers.push("John Constantine");
+activeUsers.push("Thomas Anderson");
+console.log(activeUsers);
+
+// Constant agelist ia annotated as an array of numbers with initial items
+const ageList: number[] = [45, 56, 13];
+
+console.log(ageList);
+ageList.pop();
+ageList.push(47);
+console.log(ageList);

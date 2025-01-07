d7a30cfb4c8cbe24bbf80c364030302540286fd7
benj-lazaro <benjie.work@gmail.com>
2025-01-07 16:44:49 +0800

    03 multidimensional arrays

---
+++ 06-array-types/03-multidimensional-arrays/code/array-types.js
@@ -0,0 +1,11 @@
+// Multidimensional Arrays
+// A two-dimensional array of type string
+var board = [
+    ["X", "O", "X"],
+    ["X", "O", "X"],
+    ["X", "O", "X"],
+];
+console.log(board[0]);
+// A three-dimensional array of type number
+var demo = [[[1]], [[2]]];
+console.log(demo[0][0][0]);
---
+++ 06-array-types/03-multidimensional-arrays/code/array-types.ts
@@ -0,0 +1,15 @@
+// Multidimensional Arrays
+
+// A two-dimensional array of type string
+const board: string[][] = [
+  ["X", "O", "X"],
+  ["X", "O", "X"],
+  ["X", "O", "X"],
+];
+
+console.log(board[0]);
+
+// A three-dimensional array of type number
+const demo: number[][][] = [[[1]], [[2]]];
+
+console.log(demo[0][0][0]);

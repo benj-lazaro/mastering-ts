bdb9dee1b8f6e1339ea56c340d8367a1f1b2bb56
benj-lazaro <benjie.work@gmail.com>
2025-01-11 03:06:35 +0800

    01 introducing interfaces

---
+++ 09-interfaces/01-intro-interfaces/code/interfaces.ts
@@ -0,0 +1,17 @@
+// Introducing Interfaces
+
+// Type Alias
+type TypePoint = {
+  x: number;
+  y: number;
+};
+
+// Interface
+interface InterfacePoint {
+  x: number;
+  y: number;
+}
+
+const coord1: TypePoint = { x: 31, y: 41 };
+
+const coord2: InterfacePoint = { x: 12, y: 22 };

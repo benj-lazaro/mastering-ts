76d9b964aea62c9fa0145fc2d296d7682b629727
benj-lazaro <benjie.work@gmail.com>
2025-01-11 21:34:18 +0800

    02 readonly & optional interface properties

---
+++ 09-interfaces/02-readonly-optional/code/interfaces.js
@@ -0,0 +1,10 @@
+// Readonly and Optional Interface Properties
+var person = {
+    id: 1,
+    first: "John",
+    last: "Wick",
+    nickname: "Babayaga",
+};
+console.log(person);
+person = { id: 2, first: "John", last: "Constantine" };
+console.log(person);

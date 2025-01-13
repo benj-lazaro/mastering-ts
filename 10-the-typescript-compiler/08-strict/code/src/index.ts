9186abfa97ae76065750b0cd8da0a2edf5d87d2a
benj-lazaro <benjie.work@gmail.com>
2025-01-13 18:05:23 +0800

    08 strict option

---
+++ 10-the-typescript-compiler/08-strict/code/src/index.ts
@@ -0,0 +1,22 @@
+// Working With Multiple Files
+
+interface Chicken {
+  name: string;
+  breed: string;
+  eggsPerWeek: number;
+  age: number;
+}
+
+const chooks: Chicken = {
+  name: "Chooks",
+  breed: "Silkie",
+  eggsPerWeek: 4,
+  age: 2,
+};
+
+const kentuky: Chicken = {
+  name: "Kentuky",
+  breed: "Silkie",
+  eggsPerWeek: 5,
+  age: 1,
+};

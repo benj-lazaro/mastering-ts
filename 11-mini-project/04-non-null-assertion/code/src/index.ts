aad889cf8cc2060fe89145cde3e0636601bb08cf
benj-lazaro <benjie.work@gmail.com>
2025-01-16 16:49:19 +0800

    04 typescript non-null assertion operator

---
+++ 11-mini-project/04-non-null-assertion/code/src/index.ts
@@ -0,0 +1,9 @@
+// TypeScript's Non-Null Assertion Operator
+
+// Using with a non-null assertion operator
+const btn = document.getElementById("btn")!;
+
+// Using with an optional chaining operator
+// btn?.addEventListener("click", () => alert("Clicked"));
+
+btn.addEventListener("click", () => alert("Clicked"));

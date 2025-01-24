0ac2af7c4d0aa8e940056f794501e3d01d3a77d9
benj-lazaro <benjie.work@gmail.com>
2025-01-24 13:29:02 +0800

    01 annontating classes in typescript

---
+++ 13-typescript-classes/01-annotating-classes/code/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// Annontating Classes in TypeScript
+class Player {
+    constructor(first, last) {
+        this.first = first;
+        this.last = last;
+    }
+}
+const wick = new Player("John", "Wick");
---
+++ 13-typescript-classes/01-annotating-classes/code/src/index.ts
@@ -0,0 +1,13 @@
+// Annontating Classes in TypeScript
+
+class Player {
+  first: string;
+  last: string;
+
+  constructor(first: string, last: string) {
+    this.first = first;
+    this.last = last;
+  }
+}
+
+const wick = new Player("John", "Wick");

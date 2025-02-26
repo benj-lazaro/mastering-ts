6470e89dc90233694fcd8bd8cc22532c92963866
benj-lazaro <benjie.work@gmail.com>
2025-02-26 18:49:53 +0800

    03 first component

---
+++ 19-react-and-typescript/03-first-component/shopping-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Greeter from './components/Greeter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Greeter />
+      <Greeter />
+      <Greeter />
+    </div>
+  );
+}
+
+export default App;
---
+++ 19-react-and-typescript/03-first-component/shopping-app/src/components/Greeter.tsx
@@ -0,0 +1,7 @@
+import React, { JSX } from 'react';
+
+function Greeter(): JSX.Element {
+    return <h1>Hello!</h1>
+}
+
+export default Greeter;

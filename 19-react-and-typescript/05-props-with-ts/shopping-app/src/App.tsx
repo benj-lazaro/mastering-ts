caa73a60a0d4c83b5e079f466038fcfd1145e6bc
benj-lazaro <benjie.work@gmail.com>
2025-02-27 17:49:02 +0800

    05 props with typescript

---
+++ 19-react-and-typescript/05-props-with-ts/shopping-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Greeter from './components/Greeter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Greeter person="Colt" />
+      <Greeter person="Blue" />
+      <Greeter person= "Elton" />
+    </div>
+  );
+}
+
+export default App;

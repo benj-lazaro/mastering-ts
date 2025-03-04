899246ec81944d420248a946bc0c3e50a368fc5f
benj-lazaro <benjie.work@gmail.com>
2025-03-04 18:31:25 +0800

    11 finishing touches

---
+++ 19-react-and-typescript/11-finishiing-touches/shopping-app/src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import ShoppingList from './components/ShoppingList';
+import ShoppingListForm from './components/ShoppingListForm';
+import Item from './models/item';
+import { v4 as getId } from "uuid";
+import './App.css';
+
+
+function App() {
+
+  const [items, setItems] = useState<Item[]>([], )
+  
+  const addItem = (product: string, quantity: number) => {
+    console.log("Made it to the <App> component.");
+    
+    // Adds a new item into the existing list
+    setItems([...items, {id: getId(), product: product, quantity: quantity}])
+  };
+
+  // const items = [
+  //   {id: 1, product: "Lemon", quantity: 3},
+  //   {id: 2, product: "Chicken Breast", quantity: 2},
+  // ];
+
+  return (
+  <div>
+    <ShoppingList items={items} /> 
+    <ShoppingListForm onAddItem={addItem} />
+  </div>
+);
+}
+
+export default App;
